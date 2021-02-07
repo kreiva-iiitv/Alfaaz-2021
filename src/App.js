47ac50dfcf8c71f3225b7edfb60e9baac0b8d30b
unknown <>
2021-02-07 19:44:46 +0530

    added footer and header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from ".//Pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      {/*
-      Add Header 
-
-      ---Routing---
-      <Router>
-        <Switch>
-          <Route path="/" exact component={() => <Home />} />
-        </Switch>
-      </Router>
-
-      Add Footer
-    */}
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Footer from "../Footer";
+import Header from "../Header";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
