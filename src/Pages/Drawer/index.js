35014454ced1cbb5f711783b5cff3ae71c6a4f27
Anvay <anvaymishra5@gmail.com>
2021-02-19 11:01:12 +0530

    made side drawer for smaller screens

---
+++ src/Pages/Drawer/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Drawer.css";
+import "font-awesome/css/font-awesome.min.css";
+import { NavLink } from "react-router-dom";
+
+const Drawer = (props) => {
+
+    return (
+        <div className={"SideDrawer " + (props.open ? 'Open' : 'Close')}>
+        <div onClick={props.closed}>
+            <i className="fa fa-bars side__hamburger" onClick={props.opened}></i>
+        </div>
+
+        <div className="side__tabs">
+            <div className="side__tab " >
+            <NavLink onClick={props.closed} exact activeClassName="mobile__active" style={{ textDecoration: 'none', color: 'black' }} to='/'>HOME</NavLink>
+            </div>
+            <div className="side__tab">
+            <NavLink onClick={props.closed} exact activeClassName="mobile__active" style={{ textDecoration: 'none', color: 'black' }} to='/events'>EVENTS</NavLink>
+            </div>
+            <div className="side__tab">
+            <NavLink onClick={props.closed} exact activeClassName="mobile__active" style={{ textDecoration: 'none', color: 'black' }} to='/members'>MEMBERS</NavLink>
+            </div>
+            <div className="side__tab">
+            <a onClick={props.closed} activeClassName="mobile__active" style={{ textDecoration: 'none', color: 'black' }} href="https://kreiva.tk/media/brochure.pdf">BROCHURE</a>
+            </div>
+        </div>
+
+        <div className="side__handles">
+            <div>
+                <a href="https://www.facebook.com/iiitvlitclub/" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook-square"></i></a>
+            </div>
+            <div>
+                <a href="https://www.instagram.com/alfaaz_iiitv/" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram"></i></a>
+            </div>
+            <div>
+                <a href="https://www.facebook.com/iiitvlitclub/" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter"></i></a>
+            </div>
+        </div>
+
+    </div>
+
+);
+};
+
+export default Drawer;
