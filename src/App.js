0afe10fe06ab7cec34a48a9e94c08b6e6b8129b3
anurag04-max <oanurag715@gmail.com>
2021-02-19 17:36:35 +0530

    event page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Pages/Home";
+import Events from  "./Pages/Events";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/Event" component={Events} />
       </Switch>
     </Router>
   );
--- src/Pages/Events/index.jsx
+++ src/Pages/Events/index.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import Header from "../Header";
+import Cards from "../Cards";
+import "./Events.css";
+import Cardinfo from "../../Cardinfo.js";
 
 const Events = () => {
-  return <div>Events</div>;
+  return <div>
+
+  <Header />
+
+  <div className ="heading">EVENTS </div>
+<Cards
+worth = {Cardinfo[0].worth}
+venue =  {Cardinfo[0].venue}
+date= {Cardinfo[0].date}
+convenor={Cardinfo[0].convenor}
+coconvenor={Cardinfo[0].coconvenor}
+members={Cardinfo[0].members}
+
+/>
+<Cards
+worth = {Cardinfo[1].worth}
+venue =  {Cardinfo[1].venue}
+date= {Cardinfo[1].date}
+convenor={Cardinfo[1].convenor}
+coconvenor={Cardinfo[1].coconvenor}
+members={Cardinfo[1].members}
+
+/>
+<Cards
+worth = {Cardinfo[2].worth}
+venue =  {Cardinfo[2].venue}
+date= {Cardinfo[2].date}
+convenor={Cardinfo[2].convenor}
+coconvenor={Cardinfo[2].coconvenor}
+members ={Cardinfo[2].members}
+
+/>
+
+
+  </div>;
 };
 
 export default Events;
