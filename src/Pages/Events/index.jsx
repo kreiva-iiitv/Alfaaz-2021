4d81896b1f8ca886030d9e7adf34c24677ee1065
unknown <>
2021-02-25 00:47:54 +0530

    card bottom margin updated

--- src/Pages/Events/index.jsx
+++ src/Pages/Events/index.jsx
@@ -7,21 +7,23 @@ import Footer from "../Footer";
 import NavigationHelper from "../NavigationHelper";
 
 const Events = () => {
-  return <div>
-< NavigationHelper/>
-  <div className ="heading">EVENTS </div>
-  {Cardinfo.map(card=>(
-    <Cards
-    worth = {card.worth}
-    venue =  {card.venue}
-    date= {card.date}
-    convenor={card.convenor}
-    coconvenor={card.coconvenor}
-    members={card.members}
-  />
-  ))
-}
-<Footer/>
-</div>;
+  return (
+    <div>
+      <NavigationHelper />
+      <div className="heading">EVENTS </div>
+      {Cardinfo.map((card) => (
+        <Cards
+          worth={card.worth}
+          venue={card.venue}
+          date={card.date}
+          convenor={card.convenor}
+          coconvenor={card.coconvenor}
+          members={card.members}
+        />
+      ))}
+
+      <Footer />
+    </div>
+  );
 };
 export default Events;
