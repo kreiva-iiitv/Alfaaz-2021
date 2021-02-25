a86a06f086147c62ef566d72255daf7aa4c1a695
Anvay <anvaymishra5@gmail.com>
2021-02-25 11:32:11 +0530

    added images to mobile view

--- src/Pages/Gallery/index.jsx
+++ src/Pages/Gallery/index.jsx
@@ -171,7 +171,7 @@ function Gallery() {
                   <img
                     alt="img"
                     className="mobileGallery__container__img"
-                    src={sampleImg}
+                    src={'https://firebasestorage.googleapis.com/v0/b/alfaaz-a71da.appspot.com/o/carousel%2F1-min.jpg?alt=media&token=ec758777-655c-4138-b3d0-0aa77d09f268'}
                     draggable="false"
                   />
                 </div>
@@ -181,7 +181,7 @@ function Gallery() {
                   <img
                     alt="img"
                     className="mobileGallery__container__img"
-                    src={sampleImg}
+                    src={'https://firebasestorage.googleapis.com/v0/b/alfaaz-a71da.appspot.com/o/carousel%2F6-min.jpg?alt=media&token=e5045a0b-1ee2-41c8-b6d4-910271468525'}
                     draggable="false"
                   />
                 </div>
@@ -191,7 +191,7 @@ function Gallery() {
                   <img
                     alt="img"
                     className="mobileGallery__container__img"
-                    src={sampleImg}
+                    src={'https://firebasestorage.googleapis.com/v0/b/alfaaz-a71da.appspot.com/o/carousel%2F3-min.jpg?alt=media&token=09469799-6123-45b6-8d2f-0b17b1c83dc4'}
                     draggable="false"
                   />
                 </div>
@@ -201,7 +201,7 @@ function Gallery() {
                   <img
                     alt="img"
                     className="mobileGallery__container__img"
-                    src={sampleImg}
+                    src={'https://firebasestorage.googleapis.com/v0/b/alfaaz-a71da.appspot.com/o/carousel%2F4-min.jpg?alt=media&token=44c86240-2cb9-4f69-9c09-17769027c22f'}
                     draggable="false"
                   />
                 </div>
@@ -211,7 +211,7 @@ function Gallery() {
                   <img
                     alt="img"
                     className="mobileGallery__container__img"
-                    src={sampleImg}
+                    src={'https://firebasestorage.googleapis.com/v0/b/alfaaz-a71da.appspot.com/o/carousel%2F5-min.jpg?alt=media&token=2a06d515-6b26-46f5-8b3a-d6d879aa04f8'}
                     draggable="false"
                   />
                 </div>
@@ -221,7 +221,7 @@ function Gallery() {
                   <img
                     alt="img"
                     className="mobileGallery__container__img"
-                    src={sampleImg}
+                    src={'https://firebasestorage.googleapis.com/v0/b/alfaaz-a71da.appspot.com/o/carousel%2F7-min.jpg?alt=media&token=645163a2-358b-4f96-b737-30dfad8540c3'}
                     draggable="false"
                   />
                 </div>
@@ -231,7 +231,7 @@ function Gallery() {
                   <img
                     alt="img"
                     className="mobileGallery__container__img"
-                    src={sampleImg}
+                    src={'https://firebasestorage.googleapis.com/v0/b/alfaaz-a71da.appspot.com/o/carousel%2F2-min.jpg?alt=media&token=f536d136-f687-4cb7-8a87-caa2404c3b5c'}
                     draggable="false"
                   />
                 </div>
@@ -241,7 +241,7 @@ function Gallery() {
                   <img
                     alt="img"
                     className="mobileGallery__container__img"
-                    src={sampleImg}
+                    src={'https://firebasestorage.googleapis.com/v0/b/alfaaz-a71da.appspot.com/o/carousel%2F8-min-min.jpg?alt=media&token=c05c583f-8824-40c3-85bb-61f1556a8042'}
                     draggable="false"
                   />
                 </div>
@@ -251,7 +251,7 @@ function Gallery() {
                   <img
                     alt="img"
                     className="mobileGallery__container__img"
-                    src={sampleImg}
+                    src={'https://firebasestorage.googleapis.com/v0/b/alfaaz-a71da.appspot.com/o/carousel%2F9-min-min.jpg?alt=media&token=0ef5b5e0-7c6f-401b-986a-3c4035d33e61'}
                     draggable="false"
                   />
                 </div>
@@ -261,7 +261,7 @@ function Gallery() {
                   <img
                     alt="img"
                     className="mobileGallery__container__img"
-                    src={sampleImg}
+                    src={'https://firebasestorage.googleapis.com/v0/b/alfaaz-a71da.appspot.com/o/carousel%2F10-min.jpg?alt=media&token=1c4a87c2-04c4-458a-8ffa-68826dd4631d'}
                     draggable="false"
                   />
                 </div>
@@ -271,7 +271,7 @@ function Gallery() {
                   <img
                     alt="img"
                     className="mobileGallery__container__img"
-                    src={sampleImg}
+                    src={'https://firebasestorage.googleapis.com/v0/b/alfaaz-a71da.appspot.com/o/carousel%2F11-min.jpg?alt=media&token=a0217fb4-6688-4f2c-9bb2-c3ae98fd2b6b'}
                     draggable="false"
                   />
                 </div>
@@ -281,7 +281,7 @@ function Gallery() {
                   <img
                     alt="img"
                     className="mobileGallery__container__img"
-                    src={sampleImg}
+                    src={'https://firebasestorage.googleapis.com/v0/b/alfaaz-a71da.appspot.com/o/carousel%2F12-min.jpg?alt=media&token=5da1ee47-031a-4bcb-bc15-a65e95afbec8'}
                     draggable="false"
                   />
                 </div>
