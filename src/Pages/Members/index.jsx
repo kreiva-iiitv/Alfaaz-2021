d0fb5a3424b66c52b99af32282bef869fb0c190a
Aditya Prakash <p.aditya.198@gmail.com>
2021-03-04 14:03:23 +0530

    events json added

--- src/Pages/Members/index.jsx
+++ src/Pages/Members/index.jsx
@@ -1,54 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Member from "../../Component/Member";
 import Navbar from "../../Component/Navbar";
 import "./Members.css";
 import Footer from "../Footer";
 import NavigationHelper from "../NavigationHelper";
-import Loader from "../../Component/Loader/Loader";
 import { members } from "../../json/members.json";
 
 const Members = () => {
-  const [teams, setTeams] = useState("");
   const [currentTab, setCurrentTab] = useState(0);
-  const [isLoading, setIsLoading] = useState(false);
 
   const changeCurrentTab = (tab) => {
     setCurrentTab(tab);
   };
-
-  // useEffect(() => {
-  //   async function fetchAPI() {
-  //     setIsLoading(true);
-  //     const res = await fetch("https://gaurkrishna.pythonanywhere.com/team/");
-  //     const data = await res.json();
-  //     setTeams(data.Team);
-  //     setIsLoading(false);
-  //     /*------pre loading images------*/
-  //     data.Team.map((team) =>
-  //       team.teamMembers.map((member) => {
-  //         const img = new Image();
-  //         img.src = member.profilepic;
-  //         return img;
-  //       })
-  //     );
-  //   }
-  //   fetchAPI();
-  //   window.scroll(0, 0);
-  // }, []);
-
-  // const teamNames = teams !== "" ? teams.map((team) => team.name) : null;
-
-  // const CurrentTabMembers =
-  //   teams !== ""
-  //     ? teams[currentTab].teamMembers.map((member) => (
-  //         <Member
-  //           imgSrc={member.profilepic}
-  //           name={`${member.first_name} ${member.last_name}`}
-  //           position={member.role}
-  //           linkedIn={member.linkedIn}
-  //         />
-  //       ))
-  //     : [];
 
   return (
     <React.Fragment>
@@ -59,21 +22,16 @@ const Members = () => {
         changeCurrentTab={changeCurrentTab}
       />
       <div className="members">
-        {isLoading ? (
-          <Loader></Loader>
-        ) : (
-          <React.Fragment>{members.map((ele) => {
-            return (
-              <Member
-            imgSrc={ele.img}
-            name={ele.name}
-            position={ele.role}
-            linkedIn={ele.linkedIn}
-          />
-            )
-          })}</React.Fragment>
-        )}
-        {/* <Loader></Loader> */}
+        {members.map((ele) => {
+          return (
+            <Member
+              imgSrc={ele.img}
+              name={ele.name}
+              position={ele.role}
+              linkedIn={ele.linkedIn}
+            />
+          );
+        })}
       </div>
       <Footer />
     </React.Fragment>
--- src/Pages/TimelineIndex/index.jsx
+++ src/Pages/TimelineIndex/index.jsx
@@ -1,37 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Footer from "../Footer";
 import EventHeader from "../../Component/EventHeader/EventHeader";
 import Timeline from "../../Component/Timeline/Timeline";
 import NavigationHelper from "../NavigationHelper";
-import Loader from "../../Component/Loader/Loader";
+import events from "../../json/events.json";
 
 const TimelineIndex = () => {
-  const [events, setEvents] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    async function fetchAPI() {
-      setIsLoading(true);
-      const res = await fetch("https://sheet.best/api/sheets/e7fb214f-726c-4ac7-8d84-a354edeaffc2");
-      const data = await res.json();
-      setEvents(data);
-      setIsLoading(false);
-      window.scroll(0, 0);
-    }
-    fetchAPI();
-  }, []);
-
   return (
     <React.Fragment>
       <NavigationHelper />
       <EventHeader timeline={true} />
-      {isLoading ? (
-        <div className ="Loader-Container">
-        <Loader></Loader>
-        </div>
-      ) : (
-        <Timeline events={events} />
-      )}
+      <Timeline events={events} />
       <Footer />
     </React.Fragment>
   );
